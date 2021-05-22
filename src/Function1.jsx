96be313f62991e2e2b705d6d35b2033e8c646116
Nshiv011 <61015899+Nshiv011@users.noreply.github.com>
2021-05-23 01:49:32 +0530

    b

--- src/Function1.jsx
+++ src/Function1.jsx
@@ -3,7 +3,7 @@ import { form } from 'react-bootstrap';
 
 import Modal from 'react-bootstrap/Modal';
 import { Button} from 'react-bootstrap';
-
+import { Form,Row,Col } from 'react-bootstrap';
 
 
 
@@ -41,14 +41,71 @@ function MyVerticallyCenteredModal1(props) {
     <label for="name">       </label>
     <input type="text" placeholder="Whatsapp No." name="mail" required/>
 
-    <label for="name">       </label>
-    <input type="text" placeholder="Support:" name="mail" required/>
+    <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control className="city" as="select" placeholder="Support:">
+      <option className="supp">Support:</option>
+      <option>Mental Health</option>
+      <option>Advisory</option>
+      <option>Health Bulletin of COVID-19
+For Uttarakhand</option>
+      <option>Dedicated Covid Facilities
+In Uttarakhand</option>
+<option>Corona Guideline</option>
+<option>Circulars/ Letters</option>
+<option>DENGUE Awereness
+Prevention Management</option>
+<option>Training Material</option>
+    </Form.Control>
+  </Form.Group>
 
     <label for="name">       </label>
     <input type="text" placeholder="Adress" name="mail" required/>
 
-    <label for="name">       </label>
-    <input type="text" placeholder="City" name="mail" required/>
+    <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control className="city" as="select" placeholder="City">
+      <option className="supp">City:</option>
+      <option>Almora</option>
+      <option>Badrinathpuri</option>
+      <option>Bageshwar</option>
+      <option>Bahadarabad</option>
+      <option>Bhimtal</option>
+      <option>Chakrata</option>
+      <option>Chamba</option>
+      <option>Chamoli Gopeshwar</option>
+      <option>Champawat</option>
+      <option>Clement Town</option>
+      <option>Dehradun</option>
+      <option>Devaprayag</option>
+      <option>Dharchula</option>
+      <option>Dogadda</option>
+      <option>Doiwala</option>
+      <option>Gangotri</option>
+      <option>Gochar (Gauchar)</option>
+      <option>Haldwani </option>
+      <option>Haridwar </option>
+      <option>Joshimath </option>
+      <option>Karnaprayag </option>
+      <option>Kedarnath </option>
+      <option>Khatima </option>
+      <option>Kotdwar </option>
+      <option>Lansdowne </option>
+      <option>Mussoorie </option>
+      <option>Nainital </option>
+      <option>Narendranagar </option>
+      <option>Pauri </option>
+      <option>Pithoragarh </option>
+      <option>Raipur </option>
+      <option>Ramnagar </option>
+      <option>Ranikhet </option>
+      <option>Rishikesh </option>
+      <option>Roorkee </option>
+      <option>Rudraprayag </option>
+      <option>Srinagar </option>
+      <option>Tehri </option>
+      <option>Uttarkashi </option>
+      <option>Vikasnagar </option>
+    </Form.Control>
+  </Form.Group>
 
     
     <div class="clearfix">
--- src/Function2.jsx
+++ src/Function2.jsx
@@ -4,7 +4,7 @@ import { form } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import { Button} from 'react-bootstrap';
 
-
+import { Form,Row,Col } from 'react-bootstrap';
 
 
 
@@ -42,20 +42,81 @@ import { Button} from 'react-bootstrap';
     <input type="text" placeholder="Whatsapp No." name="mail" required/>
 
     <label for="name">       </label>
-    <input type="text" placeholder="Service:" name="mail" required/>
+    <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control className="city" as="select" placeholder="Service:">
+      <option className="supp1">Service:</option>
+      <option>Mental Health</option>
+      <option>Advisory</option>
+      <option>Health Bulletin of COVID-19
+For Uttarakhand</option>
+      <option>Dedicated Covid Facilities
+In Uttarakhand</option>
+<option>Corona Guideline</option>
+<option>Circulars/ Letters</option>
+<option>DENGUE Awereness
+Prevention Management</option>
+<option>Training Material</option>
+    </Form.Control>
+  </Form.Group>
+   
 
     <label for="none">Type:</label><br/>
 
-    <input type="radio"  name="paid" />
-    <label for="pay">Paid</label>
-    <input type="radio" name="paid" />
-    <label for="pay">Free</label><br/>
+    
+    <label for="pay" className="paid"> <input type="radio"  name="paid" />Paid</label>
+    
+    <label for="pay"> <input type="radio" name="paid" /> Free</label><br/>
 
   <label for="name">       </label>
     <input type="text" placeholder="Service Location:" name="mail" required/>
 
     <label for="name">       </label>
-    <input type="text" placeholder="City:" name="mail" required/>
+    <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control className="city" as="select" placeholder="City">
+      <option className="supp1">City:</option>
+      <option>Almora</option>
+      <option>Badrinathpuri</option>
+      <option>Bageshwar</option>
+      <option>Bahadarabad</option>
+      <option>Bhimtal</option>
+      <option>Chakrata</option>
+      <option>Chamba</option>
+      <option>Chamoli Gopeshwar</option>
+      <option>Champawat</option>
+      <option>Clement Town</option>
+      <option>Dehradun</option>
+      <option>Devaprayag</option>
+      <option>Dharchula</option>
+      <option>Dogadda</option>
+      <option>Doiwala</option>
+      <option>Gangotri</option>
+      <option>Gochar (Gauchar)</option>
+      <option>Haldwani </option>
+      <option>Haridwar </option>
+      <option>Joshimath </option>
+      <option>Karnaprayag </option>
+      <option>Kedarnath </option>
+      <option>Khatima </option>
+      <option>Kotdwar </option>
+      <option>Lansdowne </option>
+      <option>Mussoorie </option>
+      <option>Nainital </option>
+      <option>Narendranagar </option>
+      <option>Pauri </option>
+      <option>Pithoragarh </option>
+      <option>Raipur </option>
+      <option>Ramnagar </option>
+      <option>Ranikhet </option>
+      <option>Rishikesh </option>
+      <option>Roorkee </option>
+      <option>Rudraprayag </option>
+      <option>Srinagar </option>
+      <option>Tehri </option>
+      <option>Uttarkashi </option>
+      <option>Vikasnagar </option>
+    </Form.Control>
+  </Form.Group>
+
   
     <div class="clearfix">
 <button type="submit" class="signupbtn1">SUBMIT </button>    
